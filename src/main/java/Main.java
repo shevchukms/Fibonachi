3bed47e802fc16109bd94b607f9c291ef546694b
Mykola Shevchuk <30670820+shevchukms@users.noreply.github.com>
2017-08-03 01:38:00 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-
         BigDecimal preLastNumber = BigDecimal.valueOf(1);
         BigDecimal lastNumber = BigDecimal.valueOf(1);
 
